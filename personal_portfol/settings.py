c46d96404b078ed1f3660611feec7002f65d503f
Andy Trushin <trand1@mail.ru>
2023-09-07 21:01:09 +0300

    Secret

--- personal_portfol/settings.py
+++ personal_portfol/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Загрузка переменных окружения из .env файла
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r1@vm3^t7bi(ygz!3e@(jmkzs86hl@y^(zffs1uty^py!%x6z^'
+# Получение секретного ключа из переменных окружения
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
